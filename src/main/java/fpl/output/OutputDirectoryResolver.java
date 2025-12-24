8ee5b99bf7d99f4e230c8f212da093f53dd8f75f
Serhii Malko <malsv@ukr.net>
2025-12-24 18:33:31 +0200

    ref OutputDirectoryResolver

--- src/main/java/fpl/output/OutputDirectoryResolver.java
+++ src/main/java/fpl/output/OutputDirectoryResolver.java
@@ -5,11 +5,16 @@ import java.io.File;
 public class OutputDirectoryResolver {
 
     public File resolve(File baseDir) {
-        if (!baseDir.exists() && !baseDir.mkdirs()) {
-            throw new RuntimeException(
-                    "Cannot create output directory: " + baseDir.getAbsolutePath()
+        if (baseDir.exists() && !baseDir.isDirectory()) {
+            throw new RuntimeException("Output path is not a directory: " + baseDir.getAbsolutePath()
             );
         }
+
+        if (!baseDir.exists() && !baseDir.mkdirs()) {
+            throw new RuntimeException("Cannot create output directory: " + baseDir.getAbsolutePath()
+            );
+        }
+
         return baseDir;
     }
 }
